0be0fed17c02287226e2264a9a75d28883b87163
alireza azizi <dev.azizimail@gmail.com>
2020-01-06 01:53:11 +0330

    create inline mode

--- src/editor.js
+++ src/editor.js
@@ -21,15 +21,42 @@ function CCEditorRemove(parent) {
         throw ('CCEdit not defined yet');
     }
 }
-
-/* under code is main function for run CCEdit */
-function CCEditRun(e) {
+/* the under function for send response to server  with post method */
+function CCEditorSave() {
+    var data = document.getElementById('cceditor-textarea');
+    var http = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+    http.onreadystatechange  = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log('sent');
+            // document.getElementById("demo").innerHTML = this.responseText;
+        }else{
+            throw('CCEditor send to save change is fail');
+        }
+    }
+    http.open('POST','https://google.com/search' , true);
+    http.send(d.data);
+}
+/**/
+/* under code is main function for run CCEdit main mode */
+function CCEditRunMainMode(e) {
     /* at first run an editor basic */
     CCEditComponent({
-        'el':'div' ,
-        'id':'cceditor' ,
-        'class':[],
-        'parent':e.main.parent,
-        'html':''
+        'el': 'div',
+        'id': 'cceditor',
+        'class': [],
+        'parent': e.main.parent,
+        'html': '<div id="cceditor-toolbar">' +
+        '<div class="cceditor-btn"><a onclick="alert(`hello owlrd`)">bold</a></div>' +
+        '<div class="cceditor-btn"><a onclick="alert(`hello owlrd`)">italic</a></div>' +
+        '<div class="cceditor-btn"><a onclick="alert(`hello owlrd`)"></a></div>' +
+        '<div class="cceditor-btn"><a onclick="alert(`hello owlrd`)">bold</a></div>' +
+        '</div>' +
+        '<div id="cceditor-textarea" contenteditable="true" spellcheck="false"><p>hello world</p></div>' +
+        '<div id="cceditor-controller">' +
+        '<div id="cceditor-controller-btn">' +
+        '<button id="cceditor-save">Save btn</button>'
+        + '</div>'
+        + '</div>'
     });
 }
+/* under code is main function for inline mode */

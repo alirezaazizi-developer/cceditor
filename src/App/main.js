64a3e12821f1057c7dab40244c24421d8d993404
alireza <dev.azizimail@gmail.com>
2020-01-15 13:56:26 +0330

    create function editor export content and some function

--- src/App/main.js
+++ src/App/main.js
@@ -1,8 +1,28 @@
 import ccEditorInline from "./run";
 
-ccEditorInline({
-    "editComponent":"" ,
-    "feature":[
-        "bold" ,
-    ]
+function contentEdit(id){
+    let component = document.getElementById(id);
+    let attribute = document.createAttribute("contenteditable");
+    attribute.value = "true";
+    component.setAttributeNode(attribute);
+}
+
+
+document.querySelector("#editable").addEventListener("click" , function () {
+    let component = document.getElementById("main");
+    let attribute = document.createAttribute("contenteditable");
+    attribute.value = "true";
+    component.setAttributeNode(attribute);
+    ccEditorInline({
+        "editComponent":"" ,
+        "feature":[
+            "bold" ,
+        ]
+    });
 });
+
+function exportContent(id) {
+    let exportContentEdited = document.getElementById(id).innerHTML;
+    console.log(exportContentEdited);
+}
+exportContent("main");
--- src/App/run.js
+++ src/App/run.js
@@ -7,15 +7,19 @@ function elementIdFinder(element) {
 
 /* under function is main part of editor */
 export default function ccEditorInline(property) {
-    let editor = document.createElement("div");
-    editor.setAttribute("id" , "ccEditor");
-    /*
-    *
-    * some code
-    *
-    */
-    property.feature.forEach(function (features) {
+    if (elementIdFinder("ccEditor") == null){
+        let editor = document.createElement("div");
+        editor.setAttribute("id" , "ccEditor");
+        /*
+        *
+        * some code
+        *
+        */
+        property.feature.forEach(function (features) {
 
-    });
-    document.body.appendChild(editor);
+        });
+        document.body.appendChild(editor);
+    }else{
+        console.log("editor exist");
+    }
 }
